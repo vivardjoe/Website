bc460ea67aed0485b8d695d21a1909534e4bf4b0
vivardjoe <118870889+vivardjoe@users.noreply.github.com>
2023-03-13 08:27:20 +0700

    Add files via upload

---
+++ dblatihan.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 12, 2023 at 03:07 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dblatihan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tsiswa`
+--
+
+CREATE TABLE `tsiswa` (
+  `id_siswa` int(11) NOT NULL,
+  `nim` varchar(9) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(128) NOT NULL,
+  `prodi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tsiswa`
+--
+
+INSERT INTO `tsiswa` (`id_siswa`, `nim`, `nama`, `alamat`, `prodi`) VALUES
+(1, '12.03.004', 'veronica', 'jakarta', 'TKJ-SMK'),
+(4, '13.76.009', 'Burhan', 'Jaharun', 'TSM-SMK'),
+(11, '12.70.677', 'kurnia', 'tanah merah', 'TKJ-SMK');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tsiswa`
+--
+ALTER TABLE `tsiswa`
+  ADD PRIMARY KEY (`id_siswa`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tsiswa`
+--
+ALTER TABLE `tsiswa`
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
